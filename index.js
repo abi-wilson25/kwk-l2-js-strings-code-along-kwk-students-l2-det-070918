d5ced940fb09ada7270b146a6388cdf69aa6dbb4
abi-wilson25 <agibailw@gmail.com>
2018-07-11 17:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = 'Welcome to Klossybook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase ()
 
-const shortGreeting = 'Welcome, ' + currentUser + '!'
+const shortGreeting = 'Welcome, ' + currentUser.splice(0) + '!'
